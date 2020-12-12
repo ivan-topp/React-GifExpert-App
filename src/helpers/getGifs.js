ee573b6899f732c3e2699d9b8c4be4516d6eb5b8
ivan-topp-espol <ivan.topp@espol.cl>
2020-12-12 14:38:46 -0300

    [INIT] First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async ( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=lzJHT70WWLtZL2DW6ParWvVu8d5lzwvE`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images?.downsized_medium.url
+    }));
+
+    return gifs;
+
+};
