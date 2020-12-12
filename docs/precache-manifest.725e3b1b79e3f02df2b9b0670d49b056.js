0c47c8f25e366c2b30d910f0abbc3f973b4d2084
ivan-topp-espol <ivan.topp@espol.cl>
2020-12-12 14:39:53 -0300

    [ADD] Added docs

---
+++ docs/precache-manifest.725e3b1b79e3f02df2b9b0670d49b056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec238c0d6ef15a85fcde909b2779461",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dd8c3fd53b9a5c33bb5",
+    "url": "/static/css/main.ee978791.chunk.css"
+  },
+  {
+    "revision": "a13e31d6699d5138f112",
+    "url": "/static/js/2.bed60b36.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.bed60b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dd8c3fd53b9a5c33bb5",
+    "url": "/static/js/main.e3907453.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
